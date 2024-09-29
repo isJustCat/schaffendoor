ab6ebb1a1a02fbf84183a9dca27915c736c96f8a
polygon <ally@critter.codes>
2024-09-29 02:12:27 +0200

    no i was right

--- doormagic.sh
+++ doormagic.sh
@@ -5,13 +5,13 @@ if [ "$USER" == "open" ]; then
     echo "Linking to DDR Mondbasis..."
     echo "Asking the party for permission to unlock door...."
     echo "Waiting for Faxmodem..."
-    echo "Chanelling the force..."
+    echo "chanelling the force"
     echo "Opening door..."
-    PIN=17 # pin 11
+    PIN=17 # pin
 elif [ "$USER" == "close" ]; then
     echo "Connecting to space systems..."
     echo "Linking to DDR Mondbasis..."
-    echo "Rickrolling folks on Mondbasis..."
+    echo "Rickrolling folks on mondbasis..."
     echo "Locating Messlatte..."
     echo "Closing door..."
     PIN=22 # pin 15
@@ -28,6 +28,4 @@ sleep 1
 
 sudo raspi-gpio set $PIN dh
 
-sudo raspi-gpio set $PIN dl
-
-echo "Action completed on GPIO pin $PIN, set back to low."
+echo "Action completed on GPIO pin $PIN"
